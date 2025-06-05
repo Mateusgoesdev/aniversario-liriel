e013aecae4352cf3c8023ba5459ff8f72a3d7de9
mateusgoesdev <mateusgoes374@gmail.com>
2025-06-05 13:43:41 -0300

    .

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function criarCoracao() {
   const heart = document.createElement('div');
   heart.classList.add('heart');
   heart.style.left = Math.random() * 100 + 'vw';
-  heart.style.animationDuration = Math.random() * 3 + 3 + 's';
+  heart.style.animationDuration = Math.random() * 2 + 2 + 's';
   container.appendChild(heart);
   
   setTimeout(() => heart.remove(), 6000);
